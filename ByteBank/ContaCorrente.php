91ef5ec08eea5f87e9d03ff32b548347fac991ae
etbastos <eduardo.tbastos@gmail.com>
2019-08-13 16:23:50 -0300

    :-)

---
+++ ByteBank/ContaCorrente.php
@@ -0,0 +1,9 @@
+<?php
+
+Class ContaCorrente{
+    public $titular;
+    public $agencia;
+    public $numero;
+    public $saldo;
+}
+?>
---
+++ ByteBank/index.php
@@ -0,0 +1,12 @@
+<?php
+//ini_set('display_errors',1);
+//error_reporting();
+//header('Content-Type: text/html; charset=utf-8');
+
+require "ContaCorrente.php";
+
+$contaJoao = new ContaCorrente();
+
+var_dump($contaJoao);
+
+?>
